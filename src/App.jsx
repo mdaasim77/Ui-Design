d56a69c5798a72b42615871651aeef9d83ad623d
mdaasim77 <khanaasim9910@gmail.com>
2026-01-28 18:11:30 +0530

    add images and create container and  box

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React from "react";
 
 export default function App() {
   return (
     <>
-      <h1 className='bg-blue-500 text-black'>To-Do List</h1>
-      <h1 className='bg-blue-500 text-black'>To-Do List</h1>
+      <div className="min-h-screen w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 flex items-center justify-center">
+        <div className="h-auto w-[30vw] bg-white rounded-lg">
+          <h1 className="font-bold text-xl flex p-5 items-center">
+            To Do List{" "}
+            <img
+              className="h-16 w-16" 
+              src="src/images/icon.png"
+              alt="icon image"
+            />
+          </h1>
+        </div>
+      </div>
     </>
-  )
+  );
 }
