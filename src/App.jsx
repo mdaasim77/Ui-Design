bec251da75a9b05e9c01eff294724f7d9a4247c9
mdaasim77 <khanaasim9910@gmail.com>
2026-01-28 18:30:48 +0530

    give design to list

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,21 @@ export default function App() {
   return (
     <>
       <div className="min-h-screen w-full bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-400 flex items-center justify-center">
-        <div className="h-auto w-[30vw] bg-white rounded-lg">
-          <h1 className="font-bold text-xl flex p-5 items-center">
-            To Do List{" "}
+        <div className="h-auto w-[30vw] bg-white rounded-lg p-5">
+          {/* todo list & image */}
+          <h1 className="font-bold text-xl flex items-center">
+            To Do List
             <img
-              className="h-16 w-16" 
+              className="h-8 w-8 mx-5"
               src="src/images/icon.png"
               alt="icon image"
             />
           </h1>
+          {/* input part */}
+          <div className="mt-5 rounded-2xl bg-gray-200 flex justify-between p-5">
+            <input className=" rounded-2xl bg-gray-200 w-[80vw]" type="text" />
+            <button className="bg-orange-400">Add</button>
+          </div>
         </div>
       </div>
     </>
